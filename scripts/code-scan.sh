752c6db63f1cdad2601376202751a5e5380ae6e5
Fraser Goffin <fraser.goffin@nationwide.co.uk>
2023-04-20 09:08:50 +0100

    code-scan.sh : experiments

--- scripts/code-scan.sh
+++ scripts/code-scan.sh
@@ -14,11 +14,36 @@ JSON_PAYLOAD="{\"ref\":\"${GH_REPO_BRANCH_REF}\",\"inputs\":{\"RUNNER_TYPE\":\"$
 
 #echo "JSON_PAYLOAD = $JSON_PAYLOAD";
 
-curl -L \
+#STATUS_CODE=$(curl -L -f -s -o /dev/null \
+
+STATUS_CODE=$(curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${BEARER_TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
+  -w '%{http_code}' \
   https://api.github.com/repos/${GH_REPO_OWNER}/${GH_REPO_NAME}/actions/workflows/${GH_WORKFLOW_ID}/dispatches \
   -g \
-  -d "$JSON_PAYLOAD";
+  -d "$JSON_PAYLOAD");
+
+echo "Return Code is - $?";
+echo "STATUS_CODE=$STATUS_CODE";
+
+#curl -L \
+#  -X POST \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: Bearer ${BEARER_TOKEN}"\
+#  -H "X-GitHub-Api-Version: 2022-11-28" \
+#  https://api.github.com/repos/${GH_REPO_OWNER}/${GH_REPO_NAME}/actions/workflows/${GH_WORKFLOW_ID}/dispatches \
+#  -g \
+#  -d "$JSON_PAYLOAD");
+
+#if [ $STATUS_CODE -eq 200 ]; then
+#  echo "Got 200! All done!"
+#  break
+#else
+#  echo "Got $STATUS_CODE : Not done yet..."
+#fi
+
+TODAY=$(date +%F);
+echo "TODAY = $TODAY";
